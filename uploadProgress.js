a8252ac310595b09e1040555ae2a10a3a7e35f73
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2013-11-20 21:50:18 +0100

    Initial commit

---
+++ uploadProgress.js
@@ -0,0 +1,44 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['timer'], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory(require('timer'));
+    } else {
+        // Browser globals (root is window)
+        root.xhrUploadProgress = factory(root.timer);
+    }
+}(this, function (timer) {
+    "use strict";
+
+    return function(xhr, callback) {
+        if (typeof xhr === 'function' && callback == null) {
+            callback = xhr;
+            xhr = new XMLHttpRequest();
+        }
+
+        if (xhr.upload) {
+            var time = timer();
+
+            xhr.upload.addEventListener('loadstart', time.start, false);
+
+            xhr.upload.addEventListener('progress', function(event) {
+                if (!event.lengthComputable) return;
+
+                var position = event.loaded || event.position;
+                var total = event.total;
+
+                callback({
+                    percent: Math.floor((position * 100) / total),
+                    time: time()
+                });
+            }, false);
+        }
+
+        return xhr;
+    };
+
+});
